137ce578ff32215b5f9eae2587a695b27ddfa5c5
mquast01 <67843403+mquast01@users.noreply.github.com>
2020-12-24 13:45:26 -0500

    "ffscore/config"

--- commands/ffscore.js
+++ commands/ffscore.js
@@ -10,7 +10,7 @@ module.exports = {
     var score_message = "```\n"
     var weekCounter = Math.round((new Date() - new Date(2020, 8, 7)) / (7 * 24 * 60 * 60 * 1000));
     var sPeriodId;
-    if(args.length == 0) {
+    if(!args.length) {
       sPeriodId = weekCounter;
     } else if (args[0] <= 0 || args[0] > weekCounter) {
       return message.channel.send('out of range');
@@ -20,7 +20,7 @@ module.exports = {
     console.log(sPeriodId);
 
     myclient.getBoxscoreForWeek({seasonId: 2020, matchupPeriodId: sPeriodId, scoringPeriodId: sPeriodId}).then((score) => {
-      score_message += "SCORE".padStart(44) + "\n";
+      score_message += "WEEK " + sPeriodId + "SCORE".padStart(37) + "\n";
       for(i = 0; i < score.length; i++){
         score_message += ((league.teams[score[i].homeTeamId -1].location + " " + league.teams[score[i].homeTeamId -1].nickname).padStart(30));
         score_message += ("  " + score[i].homeScore).padStart(10) + "   " + (score[i].awayScore + "  ").padEnd(10);
